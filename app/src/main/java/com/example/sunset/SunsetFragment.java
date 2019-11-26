cd6bf4029e9228e52db245444a137cfc45110216
Leoneed <40763564+whatakat@users.noreply.github.com>
2019-11-26 23:16:21 +0300

    add class SunsetFragment

---
+++ app/src/main/java/com/example/sunset/SunsetFragment.java
@@ -0,0 +1,24 @@
+package com.example.sunset;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class SunsetFragment extends Fragment {
+    public static SunsetFragment newInstance(){
+        return new SunsetFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_sunset, container, false);
+        return view;
+
+    }
+}
