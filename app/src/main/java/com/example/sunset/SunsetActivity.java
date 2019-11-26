a5ec5ed186cd43e94b1c0a2c14d9fe4ee4e7baa1
Leoneed <40763564+whatakat@users.noreply.github.com>
2019-11-26 22:48:07 +0300

    Initial commit

---
+++ app/src/main/java/com/example/sunset/SunsetActivity.java
@@ -0,0 +1,14 @@
+package com.example.sunset;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SunsetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
