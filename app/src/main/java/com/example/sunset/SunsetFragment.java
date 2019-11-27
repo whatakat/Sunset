30834fed8835c3ac5c537d809221c0fa6b744dc9
Leoneed <40763564+whatakat@users.noreply.github.com>
2019-11-27 20:34:46 +0300

    setOnClickListener

--- app/src/main/java/com/example/sunset/SunsetFragment.java
+++ app/src/main/java/com/example/sunset/SunsetFragment.java
@@ -25,6 +25,13 @@ public class SunsetFragment extends Fragment {
         mSceneView = view;
         mSunView = view.findViewById(R.id.sun);
         mSkyView = view.findViewById(R.id.sky);
+
+        mSceneView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startAnimation();
+            }
+        });
         return view;
 
     }
